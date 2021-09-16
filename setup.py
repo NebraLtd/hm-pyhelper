77316d323e91c6798401ca2c5382a1192762c99f
Robert Putt <rob@puttfamily.co.uk>
2021-09-16 09:06:19 +0100

    Add base JSON RPC Client (#8)
    Co-authored-by: Robert Putt <robe8437@Roberts-MacBook-Pro.local>

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.1',
+    version='0.2',
     author="Nebra Ltd",
     description="Helium Python Helper",
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
     url="https://github.com/NebraLtd/hm-pyhelper",
     install_requires=[
-        'requests>=2.26.0'
+        'requests>=2.26.0',
+        'jsonrpcclient>=3.3.6'
     ],
     project_urls={
         "Bug Tracker": "https://github.com/NebraLtd/hm-pyhelper/issues",
