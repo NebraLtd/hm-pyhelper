c0b599b022c36eb65df115dcebc3d85b511ebe26
Viktor Petersson <self@vpetersson.com>
2021-09-23 13:31:18 +0100

    Fixes permission

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.6',
+    version='0.6.1',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
