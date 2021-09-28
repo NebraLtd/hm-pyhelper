3705079359076de8cc2b468c49fc823ffc2ec979
Viktor Petersson <self@vpetersson.com>
2021-09-28 11:26:47 +0100

    Fixes linting

--- hm_pyhelper/miner_param.py
+++ hm_pyhelper/miner_param.py
@@ -51,6 +51,7 @@ def get_gateway_mfr_test_result():
         logging.error("Unable to parse JSON from gateway_mfr")
     return False
 
+
 def get_ethernet_addresses(diagnostics):
     # Get ethernet MAC and WIFI address
 
