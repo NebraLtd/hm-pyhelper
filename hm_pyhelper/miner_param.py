13e51ff9134c1e0b13cdf9e510a4c39755324757
Viktor Petersson <self@vpetersson.com>
2021-09-28 14:08:45 +0100

    Reverts changes

--- hm_pyhelper/miner_param.py
+++ hm_pyhelper/miner_param.py
@@ -28,32 +28,8 @@ def get_public_keys_rust():
     return False
 
 
-def get_gateway_mfr_test_result():
-    """
-    Run gateway_mfr test and report back.
-    """
-    direct_path = os.path.dirname(os.path.abspath(__file__))
-    gateway_mfr_path = os.path.join(direct_path, 'gateway_mfr')
-
-    try:
-        run_gateway_mfr_keys = subprocess.run(
-            [gateway_mfr_path, "test"],
-            capture_output=True,
-            check=True
-        )
-    except subprocess.CalledProcessError:
-        logging.error("gateway_mfr exited with a non-zero status")
-        return False
-
-    try:
-        return json.loads(run_gateway_mfr_keys.stdout)
-    except json.JSONDecodeError:
-        logging.error("Unable to parse JSON from gateway_mfr")
-    return False
-
-
 def get_ethernet_addresses(diagnostics):
-    # Get Ethernet and WiFi MAC
+    # Get ethernet MAC and WIFI address
 
     # The order of the values in the lists is important!
     # It determines which value will be available for which key
@@ -63,13 +39,28 @@ def get_ethernet_addresses(diagnostics):
     ]
     keys = ["E0", "W0"]
     for (path, key) in zip(path_to_files, keys):
-        if not os.path.isfile(path):
-            diagnostics[key] = False
-            logging.error("{} doesn't exist".format(path))
-            break
         try:
-            read_file = open(path)
-            diagnostics[key] = read_file.readline().strip().upper()
-        except PermissionError as e:
+            diagnostics[key] = get_mac_address(path)
+        except Exception as e:
             diagnostics[key] = False
             logging.error(e)
+
+
+def get_mac_address(path):
+    """
+    input: path to the file with the location of the mac address
+    output: A string containing a mac address
+    Possible exceptions:
+        FileNotFoundError - when the file is not found
+        PermissionError - in the absence of access rights to the file
+        TypeError - If the function argument is not a string.
+    """
+    if type(path) is not str:
+        raise TypeError("The path must be a string value")
+    try:
+        file = open(path)
+    except FileNotFoundError as e:
+        raise e
+    except PermissionError as e:
+        raise e
+    return file.readline().strip().upper()
