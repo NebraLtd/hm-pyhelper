0395241f52ffccc5ac60a79947fe4626898ba0b5
Sebastian Maj <wolftech@cogeco.ca>
2021-11-12 15:15:52 -0500

    Fix for linting

--- hm_pyhelper/miner_param.py
+++ hm_pyhelper/miner_param.py
@@ -1,4 +1,3 @@
-from logging import Logger
 import os
 import subprocess
 import json
@@ -201,12 +200,14 @@ def get_mac_address(path):
     try:
         file = open(path)
     except FileNotFoundError as e:
-        LOGGER.exception("Failed to find Miner Mac Address file at path %s" % path)
+        LOGGER.exception("Failed to find Miner \
+            Mac Address file at path %s" % path)
         raise MinerFailedToFetchMacAddress(
             "Failed to find file containing miner mac address. \
              Exception: %s" % str(e)).with_traceback(e.__traceback__)
     except PermissionError as e:
-        LOGGER.exception("Permissions invalid for Miner Mac Address file at path %s" % path)
+        LOGGER.exception("Permissions invalid for Miner \
+            Mac Address file at path %s" % path)
         raise MinerFailedToFetchMacAddress(
             "Failed to fetch miner mac address. Invalid permissions to access file. \
              Exception: %s" % str(e)).with_traceback(e.__traceback__)
