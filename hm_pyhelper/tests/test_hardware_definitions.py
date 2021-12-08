0e32911a5aabfd9dd286a6d263ab448fe0e85994
Marvin Arnold <marvin@geeky.rocks>
2021-12-08 13:13:37 -0600

    feat(hardware definitons): add FCC and ICC fields
    - Added FCC and ICC ids to variant_defintions.
    - Only set values for variants that are currently explicitly
      handled in HPT: https://github.com/NebraLtd/Hotspot-Production-Tool/blob/d948acc4245cbe66777cae26660789b249cbb349/app/main.py#L90
    - For future proofing, made FCC_IDS an array even though we currently only seem to have one
    value per device type.

---
+++ hm_pyhelper/tests/test_hardware_definitions.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from hm_pyhelper.hardware_definitions import variant_definitions
+
+
+class TestHardwareDefinitions(TestCase):
+    def test_variant_definitions(self):
+        # Not currently expecting APPNAME, GPIO_PIN_LED, or PIO_PIN_BUTTON
+        expected_fields = {
+            'FRIENDLY',
+            'SPIBUS',
+            'RESET',
+            'MAC',
+            'STATUS',
+            'BUTTON',
+            'ECCOB',
+            'TYPE',
+            'CELLULAR',
+            'FCC_IDS',
+            'CONTAINS_FCC_IDS',
+            'ICC_IDS'
+        }
+
+        for variant_name, variant_dict in variant_definitions.items():
+            variant_keys = variant_dict.keys()
+            missing_keys = expected_fields.difference(variant_keys)
+            self.assertSetEqual(missing_keys, set())
