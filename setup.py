83782eb00e4f21b575d6d38be9542f3cd9b1f3cf
Ilya Starodubtsev <ilyastarodubtsev55@gmail.com>
2022-01-14 20:36:13 +0600

    Bump setup verison package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.12.7',
+    version='0.12.8',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
