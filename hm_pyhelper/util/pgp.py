937838fec894f462d2a61d1dad6161f704a9e5ba
Kashif Iftikhar <kashif@compulife.com.pk>
2022-02-11 11:22:20 +0500

    Helper function to extract payload from PGP clearsigned message files.

---
+++ hm_pyhelper/util/pgp.py
@@ -0,0 +1,35 @@
+"""Utility code to work with GPG signed messages."""
+
+
+def get_payload_from_clearsigned_message(message: str) -> str:
+    """
+    Given a message in clearsign format removes signature and marker strings like
+    -----BEGIN PGP SIGNATURE-----, -----BEGIN PGP SIGNED MESSAGE----- to extract
+    the original payload.
+
+    :param message: The message containing the signature and the payload.
+    :return: Extracted payload as string. Calling code is responsible for
+        converting it to proper data type.
+    """
+    lines = message.strip().split('\n')
+
+    if len(lines) < 5 \
+            or lines[0] != '-----BEGIN PGP SIGNED MESSAGE-----' \
+            or lines[1].startswith('Hash:') is False:
+        raise RuntimeError("Invalid message format, no --BEGIN PGP SIGNED MESSAGE-- header")
+
+    start_idx = 3  # Payload starts from 3rd line in clearsigned messages
+    end_idx = None
+    prev_line = ""
+
+    for idx, line in enumerate(lines[3:]):
+        if line.strip() == '-----BEGIN PGP SIGNATURE-----' and prev_line.strip() == "":
+            end_idx = idx + start_idx
+            break
+
+        prev_line = line
+
+    if end_idx is None:
+        raise RuntimeError("Invalid message format, no --BEGIN PGP SIGNATURE-- section")
+
+    return "\n".join(lines[start_idx: end_idx-1])
