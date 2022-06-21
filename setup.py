13c3100c70f858747084eac7586b08bc2dcb67bc
Zhang <zpoterasu@nebra.com>
2022-06-22 00:41:57 +0800

    fix: add packaging dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.13.26',
+    version='0.13.27',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
@@ -15,6 +15,7 @@ setup(
         'retry==0.9.2',
         'base58==2.1.1',
         'protobuf==3.19.3'
+        'packaging>=21.3'
     ],
     project_urls={
         "Bug Tracker": "https://github.com/NebraLtd/hm-pyhelper/issues",
