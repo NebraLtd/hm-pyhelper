251bd96172d0dd4a9f96cd558d13c20d076da2ea
Kashif Iftikhar <kashif@compulife.com.pk>
2022-08-30 00:55:58 +0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.13.34',
+    version='0.13.35',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
