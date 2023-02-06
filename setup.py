43c24c128751927efea41473a2932a014905e3eb
Aaron Shaw <shawaj@gmail.com>
2023-02-06 03:58:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.13.50',
+    version='0.13.51',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
