8de913a9cceff74b4703a61bc5ff3e72b6bf84d3
Murat Ursavas <mursavas@nebra.com>
2023-04-10 09:35:59 +0300

    Removed grpcio requirements
    As this package creates a lot of long time build issues I've removed
    direct dependency from here to let parent app decide which grpcio
    version to use.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='hm_pyhelper',
-    version='0.14.3',
+    version='0.14.4',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
