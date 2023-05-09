be0d41e74599912bc1718da2809bb2065f09186f
Calin Crisan <ccrisan@gmail.com>
2023-05-09 23:22:29 +0300

    Bump version to 0.14.10 in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='hm_pyhelper',
-    version='0.14.9',
+    version='0.14.10',
     author="Nebra Ltd",
     author_email="support@nebra.com",
     description="Helium Python Helper",
